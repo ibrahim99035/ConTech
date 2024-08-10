b961573184928019fb5593f30d1596471e76bd36
ibrahim <ebrahimaboeita990@gmail.com>
2024-08-10 18:07:44 +0300

    Adding the overlay on contact us form

--- src/components/ChatWithUsSection.jsx
+++ src/components/ChatWithUsSection.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import whatsappicon from '/Images/Social/whatsapp.png'
 import './Styles/ChatWithUsSection.css'; 
 
 import { FaFacebookSquare, FaLinkedin, FaInstagramSquare, FaWhatsappSquare } from "react-icons/fa";
@@ -8,37 +7,40 @@ import { SiGmail } from "react-icons/si";
 
 const ChatWithUsSection = () => {
   return (
-    <div className="contact-us">
-      <h2>تواصل معنا</h2>
-      <p>نحب أن نسمع منك, تواصل معنا عبر حساباتنا على وسائل التواصل الاجتماعي, أو أرسل لنا رسالة.</p>
+    <div>
+      <div className='overlay-image'></div>
+      <div className="contact-us">
+        <h2>تواصل معنا</h2>
+        <p>نحب أن نسمع منك, تواصل معنا عبر حساباتنا على وسائل التواصل الاجتماعي, أو أرسل لنا رسالة.</p>
 
-      <div className="social-icons">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebookSquare />
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <FaSquareXTwitter />
-        </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagramSquare />
-        </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-          <FaLinkedin />
-        </a>
-        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-          <FaWhatsappSquare />
-        </a>
-        <a href="mailto:contact@example.com" target="_blank" rel="noopener noreferrer">
-          <SiGmail />
-        </a>
+        <div className="social-icons">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebookSquare />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FaSquareXTwitter />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagramSquare />
+          </a>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+            <FaWhatsappSquare />
+          </a>
+          <a href="mailto:contact@example.com" target="_blank" rel="noopener noreferrer">
+            <SiGmail />
+          </a>
+        </div>
+
+        <form className="contact-form">
+          <input type="text" placeholder="اسمكك" required />
+          <input type="email" placeholder="بيردك الالكتروني" required />
+          <textarea placeholder="رسالتك" required></textarea>
+          <button type="submit">أرسل الأن !</button>
+        </form>
       </div>
-
-      <form className="contact-form">
-        <input type="text" placeholder="اسمكك" required />
-        <input type="email" placeholder="بيردك الالكتروني" required />
-        <textarea placeholder="رسالتك" required></textarea>
-        <button type="submit">أرسل الأن !</button>
-      </form>
     </div>
   );
 }
